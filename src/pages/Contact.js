d277dac0b27e95acc8fc4b062c9d5bed4e866b7f
Daire <daire@protonmail.com>
2023-11-07 13:38:12 +0000

    Created main pages and routing

---
+++ src/pages/Contact.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
+class Contact extends Component {
+  render() {
+    return (
+      <>
+        <Container>
+        <h2>Get in touch!</h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Your name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Image from 'react-bootstrap/Image';
+
+class Home extends Component {
+  render() {
+    return (
+        <div className='slider-image'>
+            {/* <img
+              alt="Daire Hardesty"
+              src="/DCS2416.webp"
+              className="slider-image"
+              fluid
+            /> */}
+            {/* <Image src="/DCS2416.webp"  style={{ width: "500%", height: "600%" }}  className="slider-image" fluid /> */}
+        </div>
+    );
+  }
+}
+
+export default Home;
