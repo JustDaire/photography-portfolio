2b2970045508317ecffc1634d7fca6b03c40515a
Daire <daire@protonmail.com>
2023-11-07 15:55:46 +0000

    Updated overflow styling on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import Image from 'react-bootstrap/Image';
 
-class Home extends Component {
-  render() {
-    return (
-        <div className='slider-image'>
-            {/* <img
+function Home() {
+  useEffect(() => {
+    document.body.classList.add('no-scroll');
+
+
+    return () => {
+      // removing classes from body element
+      // when the component unmounts
+      document.body.classList.remove('no-scroll');
+    }
+  }, []);
+  return (
+    <div className='slider-image'>
+      {/* <img
               alt="Daire Hardesty"
               src="/DCS2416.webp"
               className="slider-image"
               fluid
             /> */}
-            {/* <Image src="/DCS2416.webp"  style={{ width: "500%", height: "600%" }}  className="slider-image" fluid /> */}
-        </div>
-    );
-  }
+      {/* <Image src="/DCS2416.webp"  style={{ width: "500%", height: "600%" }}  className="slider-image" fluid /> */}
+    </div>
+  );
 }
 
 export default Home;
