7f92a0cfcd07d8103124757d3189cd74b396958f
Daire <daire@protonmail.com>
2023-11-07 14:30:53 +0000

    Created Gallery component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home';
 import About from './pages/About';
+import Gallery from './pages/Gallery'
 import Contact from './pages/Contact';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/gallery' element={<Gallery />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
         </div>
